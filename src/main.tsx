90d031866af7bd52b83001befb0af432b0bc5aee
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-29 16:40:14 +0200

    Create ThemeProvider and import it to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 import "./index.css";
 import App from "./components/App/App";
 import { store } from "./store";
+import theme from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
