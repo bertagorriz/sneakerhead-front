7a7f91d4c388de9ccff10e780fc2b69c286601bf
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-30 16:10:33 +0200

    Create LoginPage page and add its route to appRouter

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ContainerStyled from "../shared/ContainerStyled";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 
 const Layout = (): React.ReactElement => {
+  const location = useLocation();
+
   return (
     <>
       <Header />
       <ContainerStyled>
         <Outlet />
       </ContainerStyled>
-      <Navbar />
+      {location.pathname !== "/login" && <Navbar />}
     </>
   );
 };
