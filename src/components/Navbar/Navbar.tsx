9a7445c5fbf0d0876b1981188684ce02222eccf8
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-30 17:32:28 +0200

    Create missing paths of home and addSneakers

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import NavbarStyled from "./NavbarStyled";
+import paths from "../../routers/paths/paths";
 
 const Navbar = (): React.ReactElement => {
   return (
@@ -8,14 +9,18 @@ const Navbar = (): React.ReactElement => {
         <li>
           <NavLink
             className="navbar-list__icon"
-            to={"/add-sneaker"}
+            to={paths.addSneaker}
             aria-label="add-sneaker"
           >
             <img src="./img/add.svg" alt="add icon" width={48} height={48} />
           </NavLink>
         </li>
         <li>
-          <NavLink className="navbar-list__icon" to={"/home"} aria-label="home">
+          <NavLink
+            className="navbar-list__icon"
+            to={paths.home}
+            aria-label="home"
+          >
             <img src="./img/home.svg" alt="home icon" width={48} height={48} />
           </NavLink>
         </li>
--- src/routers/paths/paths.ts
+++ src/routers/paths/paths.ts
@@ -2,6 +2,8 @@ import PathsStructure from "./types";
 
 const paths: PathsStructure = {
   login: "/login",
+  addSneaker: "/add-sneaker",
+  home: "/home",
 };
 
 export default paths;
