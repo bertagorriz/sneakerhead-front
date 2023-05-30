f9bad2e2f440abe9e8db8b17258a71063696d064
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-30 17:10:09 +0200

    Create LoginPageStyled

---
+++ src/pages/LoginPage/LoginPageStyled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const LoginPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 24px;
+
+  .title {
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+    font-size: 1rem;
+  }
+`;
+
+export default LoginPageStyled;
