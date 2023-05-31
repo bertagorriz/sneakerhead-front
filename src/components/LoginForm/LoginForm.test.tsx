65729e92bc9313a4a77a88495998afa5ebdc9175
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-31 12:10:15 +0200

    Test enable button from LoginForm

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,13 +1,15 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import LoginForm from "./LoginForm";
 
 describe("Given a LoginForm component", () => {
+  const usernameLabel = "Username";
+  const passwordLabel = "Password";
+  const buttonText = "Login";
+
   describe("When it is rendered", () => {
     test("Then it should show an input with the label text username and another one wit the label text password", () => {
-      const usernameLabel = "Username";
-      const passwordLabel = "Password";
-
       renderWithProviders(wrapWithRouter(<LoginForm />));
 
       const usernameInput = screen.getByLabelText(usernameLabel);
@@ -18,13 +20,27 @@ describe("Given a LoginForm component", () => {
     });
 
     test("Then it should show a button with the text login", () => {
-      const buttonText = "Login";
-
       renderWithProviders(wrapWithRouter(<LoginForm />));
 
       const button = screen.getByRole("button", { name: buttonText });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and it has a username and a password", () => {
+    test("Then is should show the login button enabled", async () => {
+      renderWithProviders(wrapWithRouter(<LoginForm />));
+
+      const usernameInput = screen.getByLabelText(usernameLabel);
+      const passwordInput = screen.getByLabelText(passwordLabel);
+
+      await userEvent.type(usernameInput, "Berta");
+      await userEvent.type(passwordInput, "hola");
+
+      const button = screen.getByRole("button", { name: buttonText });
+
+      expect(button).toBeEnabled();
     });
   });
 });
