6b9d31e89ae23eff0f80d67fda9d99c9c2eef8be
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-31 15:04:51 +0200

    Create useLocalStorage custom hook

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -4,13 +4,16 @@ import { UserCredentials } from "../../store/user/types";
 import LoginPageStyled from "./LoginPageStyled";
 import paths from "../../routers/paths/paths";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
 
 const LoginPage = (): React.ReactElement => {
+  const { setToken } = useLocalStorage();
   const { getUserToken } = useUser();
   const Navigate = useNavigate();
 
   const onSubmit = async (userCredentials: UserCredentials) => {
-    const token = getUserToken(userCredentials);
+    const token = await getUserToken(userCredentials);
+    setToken("token", token);
     Navigate(paths.home, { replace: true });
 
     return token;
