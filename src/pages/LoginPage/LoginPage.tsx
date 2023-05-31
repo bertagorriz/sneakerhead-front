4f67e8d0bb47f674e92cc0037959a0e4d55cd282
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-31 20:35:43 +0200

    Create useToken custom hook and add it to LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -5,18 +5,29 @@ import LoginPageStyled from "./LoginPageStyled";
 import paths from "../../routers/paths/paths";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
+import useToken from "../../hooks/useToken/useToken";
+import { useAppDispatch } from "../../store";
+import { loginUserActionCreator } from "../../store/user/userSlice";
 
 const LoginPage = (): React.ReactElement => {
   const { setToken } = useLocalStorage();
   const { getUserToken } = useUser();
+  const { getTokenData } = useToken();
+  const dispatch = useAppDispatch();
   const Navigate = useNavigate();
 
   const onSubmit = async (userCredentials: UserCredentials) => {
     const token = await getUserToken(userCredentials);
-    setToken("token", token);
-    Navigate(paths.home, { replace: true });
 
-    return token;
+    if (token) {
+      const userData = getTokenData(token);
+
+      dispatch(loginUserActionCreator(userData));
+
+      setToken("token", token);
+
+      Navigate(paths.home, { replace: true });
+    }
   };
 
   return (
