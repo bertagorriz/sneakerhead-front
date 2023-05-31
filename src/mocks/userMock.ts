9065fc7b6900886a3cb5f4de73d0fa9249e641b4
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-31 13:55:50 +0200

    Test useUser customHook

--- src/mocks/userMock.ts
+++ src/mocks/userMock.ts
@@ -1,7 +1,12 @@
-import { UserTokenStructure } from "../store/user/types";
+import { UserCredentials, UserTokenStructure } from "../store/user/types";
 
 export const userMock: UserTokenStructure = {
   id: "13",
   name: "Berta",
   token: "asdgfhhag48hawjdg49",
+};
+
+export const userCredentialsMock: UserCredentials = {
+  username: "Berta",
+  password: "hola",
 };
