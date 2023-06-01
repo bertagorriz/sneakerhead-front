d7d75e8d0394a1efad362aecd235327294545793
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-01 12:02:51 +0200

    Add getToken and dispatch in app to check if token

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,25 @@
+import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
+import useToken from "../../hooks/useToken/useToken";
+import { useAppDispatch } from "../../store";
+import { loginUserActionCreator } from "../../store/user/userSlice";
 import Layout from "../Layout/Layout";
+import { useEffect } from "react";
 
 const App = (): React.ReactElement => {
+  const { getToken } = useLocalStorage();
+  const { getTokenData } = useToken();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const token = getToken("token");
+
+    if (token) {
+      const userData = getTokenData(token);
+
+      dispatch(loginUserActionCreator(userData));
+    }
+  }, [getToken, getTokenData, dispatch]);
+
   return <Layout />;
 };
 
