e8b950c54e1648088b9514a77892be3b269dee69
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-02 11:54:42 +0200

    Test logout function from Navbar

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,19 +1,57 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Navbar from "./Navbar";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import { RouteObject, RouterProvider, createMemoryRouter } from "react-router";
+import paths from "../../routers/paths/paths";
+import LoginPage from "../../pages/LoginPage/LoginPage";
 
 describe("Given a Navbar component", () => {
   describe("When it is rendered", () => {
-    test("Then it should show the add, home and logout links", () => {
+    test("Then it should show the add links", () => {
       renderWithProviders(wrapWithRouter(<Navbar />));
 
       const addLink = screen.getByRole("link", { name: "add-sneaker" });
-      const homeLink = screen.getByRole("link", { name: "home" });
-      const logoutButton = screen.getByRole("button", { name: "logout" });
 
       expect(addLink).toBeInTheDocument();
+    });
+
+    test("Then it should show the home link", () => {
+      renderWithProviders(wrapWithRouter(<Navbar />));
+
+      const homeLink = screen.getByRole("link", { name: "home" });
+
       expect(homeLink).toBeInTheDocument();
+    });
+
+    test("Then it should show the logout button", () => {
+      renderWithProviders(wrapWithRouter(<Navbar />));
+
+      const logoutButton = screen.getByRole("button", { name: "logout" });
+
       expect(logoutButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user is logged and clicks on the logout button", () => {
+    test("Then it should redirect the user to the loginPage", async () => {
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <Navbar />,
+          children: [{ path: "/login", element: <LoginPage /> }],
+        },
+      ];
+
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const button = screen.getByLabelText("logout");
+
+      await userEvent.click(button);
+
+      expect(router.state.location.pathname).toBe(paths.login);
     });
   });
 });
