2fbeee39a24452422969d8b3d5a657cba90b93d9
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-05 20:51:43 +0200

    Add Loader dispatch at useUser custom hook

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,16 +1,27 @@
 import axios from "axios";
 import { UserCredentials } from "../../store/user/types";
+import { useAppDispatch } from "../../store";
+import {
+  hideLoaderActionCreator,
+  showLoaderActionCreator,
+} from "../../store/ui/uiSlice";
 
 const useUser = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
+
+  const dispatch = useAppDispatch();
 
   const getUserToken = async (
     userCredentials: UserCredentials
   ): Promise<string> => {
     try {
+      dispatch(showLoaderActionCreator());
+
       const {
         data: { token },
       } = await axios.post(`${apiUrl}/user/login`, userCredentials);
+
+      dispatch(hideLoaderActionCreator());
 
       return token;
     } catch {
