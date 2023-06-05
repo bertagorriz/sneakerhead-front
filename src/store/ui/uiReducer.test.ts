0834ebf985757c096987f1ee07d066b4a07eeff9
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-05 19:37:18 +0200

    Test hideLoader reducer

--- src/store/ui/uiReducer.test.ts
+++ src/store/ui/uiReducer.test.ts
@@ -1,16 +1,33 @@
 import { UiStateStructure } from "./types";
-import { showLoaderActionCreator, uiReducer } from "./uiSlice";
+import {
+  hideLoaderActionCreator,
+  showLoaderActionCreator,
+  uiReducer,
+} from "./uiSlice";
+
+const trueLoadingState: UiStateStructure = { isLoading: true };
+const falseLoadingState: UiStateStructure = { isLoading: false };
 
 describe("Given a showLoading reducer", () => {
   describe("When it receives a current state set to false and a showLoading action with a new state", () => {
     test("Then it should return a new current state set to true", () => {
-      const currentLoadingState: UiStateStructure = { isLoading: false };
-      const expectedLoadingState: UiStateStructure = { isLoading: true };
-
       const action = showLoaderActionCreator();
-      const newLoadingState = uiReducer(currentLoadingState, action);
 
-      expect(newLoadingState).toStrictEqual(expectedLoadingState);
+      const newLoadingState = uiReducer(falseLoadingState, action);
+
+      expect(newLoadingState).toStrictEqual(trueLoadingState);
+    });
+  });
+});
+
+describe("Given a hideLoading reducer", () => {
+  describe("When it receives a current state set to true and a hideLoading action with a new state", () => {
+    test("Then it should return a new current state set to false", () => {
+      const action = hideLoaderActionCreator();
+
+      const newLoadingState = uiReducer(trueLoadingState, action);
+
+      expect(newLoadingState).toStrictEqual(falseLoadingState);
     });
   });
 });
