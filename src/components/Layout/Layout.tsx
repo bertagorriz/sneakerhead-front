d1d19f56bf2f4619a4edd297bcdfa40a8f74bf31
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-05 20:49:26 +0200

    Add conditional rendering of Loader at Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,13 +3,17 @@ import ContainerStyled from "../shared/ContainerStyled";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import paths from "../../routers/paths/paths";
+import { useAppSelector } from "../../store";
+import Loader from "../Loader/Loader";
 
 const Layout = (): React.ReactElement => {
   const location = useLocation();
+  const { isLoading } = useAppSelector((store) => store.uiStore);
 
   return (
     <>
       <Header />
+      {isLoading && <Loader />}
       <ContainerStyled>
         <Outlet />
       </ContainerStyled>
