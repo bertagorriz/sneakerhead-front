03a14b9539c28ee7ef990a6c542bf7dd7d7ef201
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-06 13:06:25 +0200

    Create ModalStyled and add it to Modal

---
+++ src/components/Modal/ModalStyled.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+const ModalStyled = styled.div`
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.color.secondary}d8;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+
+  .modal {
+    display: flex;
+    justify-content: center;
+    align-items: end;
+    position: relative;
+    padding: 1rem;
+    width: 272px;
+    height: 144px;
+
+    &__close {
+      background-color: transparent;
+      position: absolute;
+      margin: 1rem;
+      top: 0;
+      right: 0;
+    }
+
+    &--correct {
+      background-color: ${(props) => props.theme.color.goodFeedback};
+    }
+
+    &--wrong {
+      background-color: ${(props) => props.theme.color.wrongFeedback};
+    }
+
+    &__text {
+      font-size: ${(props) => props.theme.fontSize.title};
+    }
+  }
+`;
+
+export default ModalStyled;
