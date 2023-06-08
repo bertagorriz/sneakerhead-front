584589e4b2e8d65db78c2ad364e9b89700a52299
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-08 18:25:22 +0200

    Test deleteSneakers reducer

--- src/store/sneakers/sneakersReducer.test.ts
+++ src/store/sneakers/sneakersReducer.test.ts
@@ -1,7 +1,9 @@
 import { getSneakersDataMock } from "../../mocks/factories/sneakersFactory";
-import { sneakersReducer } from "./sneakersSlice";
+import { deleteSneakersActionCreator, sneakersReducer } from "./sneakersSlice";
 import { loadSneakersActionCreator } from "./sneakersSlice";
 import { SneakersStateStructure } from "./types";
+
+const sneakersList = getSneakersDataMock(5);
 
 describe("Given a loadSneakers reducer", () => {
   describe("When it receives an empty current state and loadSneakers action with a list of 5 sneakers", () => {
@@ -9,7 +11,6 @@ describe("Given a loadSneakers reducer", () => {
       const initialSneakersState: SneakersStateStructure = {
         sneakers: [],
       };
-      const sneakersList = getSneakersDataMock(5);
       const expectedSneakersState: SneakersStateStructure = {
         sneakers: sneakersList,
       };
@@ -20,6 +21,23 @@ describe("Given a loadSneakers reducer", () => {
       );
 
       expect(expectedSneakersState).toStrictEqual(newSneakersState);
+    });
+  });
+});
+
+describe("Given a deleteSneakers reducer", () => {
+  describe("When it receives a curent state with a list of 5 sneakers and a deleteSneakers action with a valid sneaker id", () => {
+    test("Then it should return the list without the sneaker matched with the id", () => {
+      const initialSneakersState: SneakersStateStructure = {
+        sneakers: sneakersList,
+      };
+
+      const newSneakersState = sneakersReducer(
+        initialSneakersState,
+        deleteSneakersActionCreator(sneakersList[0].id)
+      );
+
+      expect(newSneakersState.sneakers).toHaveLength(4);
     });
   });
 });
