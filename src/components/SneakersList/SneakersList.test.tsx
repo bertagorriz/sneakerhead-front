e14256142410de7e51bcb1749da89425f9cec50b
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-09 00:59:26 +0200

    Test handleOnSubmit card function at SneakerCard and SneakersList

--- src/components/SneakersList/SneakersList.test.tsx
+++ src/components/SneakersList/SneakersList.test.tsx
@@ -1,7 +1,9 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { getSneakersDataMock } from "../../mocks/factories/sneakersFactory";
-import { renderWithProviders } from "../../utils/testUtils";
+import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import SneakersList from "./SneakersList";
+import ListPage from "../../pages/ListPage/ListPage";
 
 describe("Given a SneakersList component", () => {
   describe("When it is rendered", () => {
@@ -17,6 +19,26 @@ describe("Given a SneakersList component", () => {
       });
 
       expect(expectedTitle).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with a list of sneakers and the user clicks on the delete button of a sneaker", () => {
+    test("Then it should remove the clicked sneaker from the list", async () => {
+      const sneakersList = getSneakersDataMock(3);
+      const buttonText = "delete";
+
+      renderWithProviders(wrapWithRouter(<ListPage />), {
+        senakersStore: { sneakers: sneakersList },
+      });
+
+      const title = screen.getByRole("heading", { name: sneakersList[0].name });
+      const button = screen.getAllByRole("button", {
+        name: buttonText,
+      });
+
+      await userEvent.click(button[0]);
+
+      expect(title).not.toBeInTheDocument();
     });
   });
 });
