2c8bd867ec923437a70bd5bc19ffc7d344714f30
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-09 12:58:49 +0200

    Test conditional delete button rendering at SneakerCard and SneakersList

--- src/components/SneakerCard/SneakerCard.test.tsx
+++ src/components/SneakerCard/SneakerCard.test.tsx
@@ -1,26 +1,31 @@
 import { screen } from "@testing-library/dom";
-import userEvent from "@testing-library/user-event";
 import { getSneakerDataMock } from "../../mocks/factories/sneakersFactory";
 import { SneakerStructure } from "../../store/sneakers/types";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import SneakerCard from "./SneakerCard";
-import ListPage from "../../pages/ListPage/ListPage";
 
 describe("Given a SneakerCard component", () => {
-  const sneakerMock: SneakerStructure = getSneakerDataMock({
+  const sneakerMockDetails: SneakerStructure = getSneakerDataMock({
     name: "990v5",
     brand: "New Balance",
     image: "Model 990v5 from New Balance",
+    user: "1",
   });
 
   describe("When it is rendered", () => {
     test("Then it should show a heading with '990v5' text", () => {
       renderWithProviders(
-        wrapWithRouter(<SneakerCard sneaker={sneakerMock} isLazy={"eager"} />)
+        wrapWithRouter(
+          <SneakerCard
+            sneaker={sneakerMockDetails}
+            isLazy={"eager"}
+            userId={"1"}
+          />
+        )
       );
 
       const expectedTitle = screen.getByRole("heading", {
-        name: sneakerMock.name,
+        name: sneakerMockDetails.name,
       });
 
       expect(expectedTitle).toBeInTheDocument();
@@ -28,44 +33,58 @@ describe("Given a SneakerCard component", () => {
 
     test("Then it should show a span with 'New Balance' brand", () => {
       renderWithProviders(
-        wrapWithRouter(<SneakerCard sneaker={sneakerMock} isLazy={"lazy"} />)
+        wrapWithRouter(
+          <SneakerCard
+            sneaker={sneakerMockDetails}
+            isLazy={"lazy"}
+            userId={"1"}
+          />
+        )
       );
 
-      const expectedSpan = screen.getByText(sneakerMock.brand);
+      const expectedSpan = screen.getByText(sneakerMockDetails.brand);
 
       expect(expectedSpan).toBeInTheDocument();
     });
 
     test("Then it should show a 990v5 NewBalance image", () => {
       renderWithProviders(
-        wrapWithRouter(<SneakerCard sneaker={sneakerMock} isLazy={"lazy"} />)
+        wrapWithRouter(
+          <SneakerCard
+            sneaker={sneakerMockDetails}
+            isLazy={"lazy"}
+            userId={"1"}
+          />
+        )
       );
 
       const expectedImage = screen.getByRole("img", {
-        name: sneakerMock.image,
+        name: sneakerMockDetails.image,
       });
 
       expect(expectedImage).toBeInTheDocument();
     });
   });
 
-  describe("When it is rendered and the delete button is clicked on", () => {
-    test("Then it shouldn't show the delete button", async () => {
-      const expectedTitle = sneakerMock.name;
-      const buttonText = "delete";
+  describe("When it is rendered and the user id matches with an id card", () => {
+    test("Then it should show a 'delete' button", () => {
+      const expectedButtonText = "delete";
 
-      renderWithProviders(wrapWithRouter(<ListPage />), {
-        senakersStore: { sneakers: [sneakerMock] },
+      renderWithProviders(
+        wrapWithRouter(
+          <SneakerCard
+            sneaker={sneakerMockDetails}
+            isLazy={"eager"}
+            userId={"1"}
+          />
+        )
+      );
+
+      const expectedButton = screen.getByRole("button", {
+        name: expectedButtonText,
       });
 
-      const title = screen.getByRole("heading", { name: expectedTitle });
-      const button = screen.getByRole("button", {
-        name: buttonText,
-      });
-
-      await userEvent.click(button);
-
-      expect(title).not.toBeInTheDocument();
+      expect(expectedButton).toBeInTheDocument();
     });
   });
 });
