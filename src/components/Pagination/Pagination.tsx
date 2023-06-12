b235b9a507f091aac5da15ebe63db1c204096076
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-12 22:47:48 +0200

    Add actionOnClick at Pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,13 +1,18 @@
 import Button from "../Button/Button";
 import PaginationStyled from "./PaginationStyled";
 
-const Pagination = (): React.ReactElement => {
+interface PaginationProps {
+  actionOnClick: () => void;
+}
+
+const Pagination = ({ actionOnClick }: PaginationProps): React.ReactElement => {
   return (
     <PaginationStyled>
       <Button
         className="pagination"
         ariaLabel="pagination"
         text="Load more ..."
+        actionOnClick={actionOnClick}
       />
     </PaginationStyled>
   );
