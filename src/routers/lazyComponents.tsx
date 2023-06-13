47c49c49d87ce2b2abfde5df99571508201f1046
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-13 18:10:37 +0200

    Create LazyNotFoundPage and add it to appRouter

--- src/routers/lazyComponents.tsx
+++ src/routers/lazyComponents.tsx
@@ -1,9 +1,14 @@
 import { lazy } from "react";
 
 export const LazyLoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+
 export const LazySneakerListPage = lazy(
   () => import("../pages/SneakerListPage/SneakerListPage")
 );
 export const LazyAddSneakerPage = lazy(
   () => import("../pages/AddSneakerPage/AddSneakerPage")
+);
+
+export const LazyNotFoundPage = lazy(
+  () => import("../pages/NotFoundPage/NotFoundPage")
 );
